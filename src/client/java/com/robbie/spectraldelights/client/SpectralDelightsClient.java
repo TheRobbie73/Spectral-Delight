368137e21291c397b5e37b2700e189dadb684f5c
TheRobbie72 <franc.mer2022@gmail.com>
2025-01-15 17:18:58 +0800

    mod setup

---
+++ src/client/java/com/robbie/spectraldelights/client/SpectralDelightsClient.java
@@ -0,0 +1,10 @@
+package com.robbie.spectraldelights.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpectralDelightsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/robbie/spectraldelights/SpectralDelights.java
@@ -0,0 +1,10 @@
+package com.robbie.spectraldelights;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpectralDelights implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
